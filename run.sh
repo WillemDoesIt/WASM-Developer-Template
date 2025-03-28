4a20dc77f082d85d189f4e2e80381aa3ef666fac
Willem Van Zwol <willem@vanzwol.com>
2025-03-27 18:39:22 -0700

    better terminal for run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
-cargo build --target wasm32-unknown-unknown --release 
+echo "cleaning out target/"
+cargo clean
+
+echo "building wasm"
+cargo build --target wasm32-unknown-unknown --release -q
+
+echo "loading wasm into web/"
 wasm-bindgen target/wasm32-unknown-unknown/release/rust_wasm.wasm --out-dir web/wasm --target web
 
+echo "setting up localhost"
 if [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$MSYSTEM" == "MINGW64" ]]; then
     # if using windows enable python hosting
     export PATH=/c/Users/$(whoami)/AppData/Local/Programs/Python/Python313:$PATH
